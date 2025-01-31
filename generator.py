6135a94bab920e6a7014883491098d13bec45c02
Aqua-16 <aquafrost16@gmail.com>
2025-01-31 14:24:22 +0530

    Create generator model

---
+++ generator.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+from torchinfo import summary
+
+class Generator(nn.Module):
+    def __init__(self, noise_dim = 100, feature_maps = 64):
+        super(Generator, self).__init__()
+        
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(noise_dim, feature_maps * 8, 4, 2, 0, bias=False),
+            nn.BatchNorm2d(feature_maps * 8),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(feature_maps * 8, feature_maps * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_maps * 4),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(feature_maps * 4, feature_maps * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_maps * 2),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(feature_maps * 2, feature_maps, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_maps),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(feature_maps, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+        
+        self._init_weights()
+
+    def _init_weights(self):
+        
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, mean=0, std=0.02)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, mean=1, std=0.02)
+                nn.init.constant_(m.bias, 0)
+    
+    def forward(self, x):
+        return self.model(x)
+    
+
+#Test
+
+if __name__ == '__main__':
+    gen = Generator()
+    z = torch.randn(64, 100)
+    z = z.view(z.size(0), 100, 1, 1) 
+
+    summary(gen, (64, 100, 1, 1))
